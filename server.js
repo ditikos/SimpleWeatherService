e1e05d4e36f770bdda1a30f633d7d2e3548a568a
Onoufrios Fakelakis <chibioni@gmail.com>
2017-05-20 19:14:25 +0300

    Changed port

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//
+//
+//
+//
+//
 var express = require('express');
 var bodyParser = require('body-parser');
 var axios = require('axios');
 
 var app = express();
-
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-    console.log(req.body);
+    console.log(JSON.stringify(req.body, undefined, 2));
 
     var location = req.body.result.parameters.location.city;
     console.log(location);
@@ -49,46 +53,7 @@ app.post('/webhook', (req, res) => {
     });
 });
 
-// GET /weather/city
-app.get('/weather/:city', (req, res) => {
-    var location = req.params.city;
-
-    console.log(location);
-
-    // url encode the location
-    var encodedAddress = encodeURIComponent(location);
-    var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-
-    // first do a reverse geolocation from the location given
-    return axios.get(geocodeURL).then((response) => {
-        if (response.data.status === 'ZERO_RESULTS') {
-            throw new Error('Unable to find that address');
-        }
-        var lat = response.data.results[0].geometry.location.lat;
-        var lng = response.data.results[0].geometry.location.lng;
-
-        // then do a weather forecast search to forecast.io
-        var weatherURL = `https://api.darksky.net/forecast/39b78cd34c557b7ac589472a5c6a9c6b/${lat},${lng}?units=si`;
-        return axios.get(weatherURL);
-    }).then((response) => {
-        // if the chained Promise succeeds then set the temperature to the context object
-        var temperature = response.data.currently.temperature;
-        var apparentTemperature = response.data.currently.apparentTemperature;
-
-        // Generate context and content in natural language
-        res.send(`The weather in ${location} would be ${temperature} degrees.`);
-    }).catch((e) => {
-        // Error check for both google reverse code and forecast.io
-        if (e.code === 'ENOTFOUND') {
-            console.log('Unable to connect to API server');
-        } else {
-            console.log(e.message);
-        }
-        res.status(404).send(e);
-    });
-});
-
-app.listen(3000, () => {
+app.listen(9000, () => {
     console.log(`Started on Port 3000.`);
 });
 
